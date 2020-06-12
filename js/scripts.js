190ca573ac90a6db2ffc7cfc5b9f3342f417c91c
Tristan Emmerson <tristan@stickerslug.com>
2020-06-12 12:33:12 -0700

    Add javascript code to take user input from form field and assign to new array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 //BUSINESS LOGIC
+let humanArray = []
 let humanInput = function(input) {
-  let humanArray = []
   for (var i = 1; i <= input; i++) {
       humanArray.push(i)
   }
   return humanArray
 }
 let intermediateSentence = function(input) {
-  ??????
-  ??????
-  ??????
-}
+  let numArray = humanInput(input);
+  let text = ""
+  for (var i = 1; i <= numArray.length; i++) {
+/* add function to loop and check chars at this point*/
+  
+  }
+  console.log(numArray);
+};
 
 
 //USER LOGIC
