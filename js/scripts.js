0df8e8ca1c42d6b3d0c9ae64036d97e67fe354e8
Tristan Emmerson <tristan@stickerslug.com>
2020-06-14 14:24:21 -0700

    Add viewport header for styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,4 @@
 //BUSINESS LOGIC
-
-const cards = document.querySelector(".cards");
-const images = document.querySelectorAll(".card__img");
-const backgrounds = document.querySelectorAll(".card__bg");
-const range = 40;
-
-
-const calcValue = (a, b) => (a/b*range-range/2).toFixed(1) 
-
-let timeout;
-document.addEventListener('mousemove', ({x, y}) => {
-  if (timeout) {
-    window.cancelAnimationFrame(timeout);
-  }
-  	
-  timeout = window.requestAnimationFrame(() => {
-    const yValue = calcValue(y, window.innerHeight);
-    const xValue = calcValue(x, window.innerWidth);
-
-    cards.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
-
-    [].forEach.call(images, (image) => {
-      image.style.transform = `translateX(${-xValue}px) translateY(${yValue}px)`;
-    });
-
-    [].forEach.call(backgrounds, (background) => {
-      background.style.backgroundPosition = `${xValue*.45}px ${-yValue*.45}px`;
-    })
-	})
-}, false);
-
 var listSentence = function(input) {
 var numbers = []
 for (var i = 1; i <= input; i++) {
